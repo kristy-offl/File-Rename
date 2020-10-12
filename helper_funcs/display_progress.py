f013aade7b59696ff1e67bb188ef652f883a678d
Gowtham0625 <70581186+Gowtham0625@users.noreply.github.com>
2020-10-12 06:25:29 +0530

    Update display_progress.py

--- helper_funcs/display_progress.py
+++ helper_funcs/display_progress.py
@@ -47,7 +47,8 @@ async def progress_for_pyrogram(
             ''.join(["□" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "<b>✅Completed:</b>{0} \n<b>📁Total Size:</b> {1}\n<b>🚀Speed:</b> {2}/s\n<b>⌚️ETA:</b> {3}\n".format(
+        tmp = progress + "<b>✅Completed:</b>{0} \n<b>📁Total Size:</b> {1}\n<b>🚀Speed:</b> {2}/s\n<b>⌚️ETA:</b> {3}\n
+        <b> 📬 Powered By : @WolfProjects </>".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
